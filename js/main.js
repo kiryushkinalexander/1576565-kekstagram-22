8e17206a730b769a36115547ebb2ff3d1f713a47
Alexander <alexanderkiryushkin@yandex.ru>
2021-02-03 15:29:48 +0300

    правки

--- js/main.js
+++ js/main.js
@@ -1,29 +1,25 @@
-// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-
-let getRandomNumber = (min, max) => {
-  if (min < 0 | max < 0) {
-    return alert('Введите число больше 0');
+const getRandomNumberInRange = (min, max) => {
+  if (min < 0 || max < 0 || max === min) {
+    // eslint-disable-next-line no-console
+    console.log('Укажите числа больше 0');
+    return;
   }
+
   if (min > max) {
     [min, max] = [max, min];
   }
+
   min = Math.ceil(min);
   max = Math.floor(max);
-  return alert(Math.floor(Math.random() * (max - min + 1)) + min);
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-getRandomNumber(0, 100);
+getRandomNumberInRange(0, 100);
 
 
-// 2 задание Функция для проверки максимальной длины строки. Будет использоваться для проверки длины введённого комментария, но должна быть универсальна
-
-
-let getCommmentLength = (validateString, maxLength) => {
-  if (validateString.length <= maxLength) {
-    return true;
-  }
-  return false;
+const validateStringLength = (validateString, maxLength) => {
+  return validateString.length <= maxLength;
 }
 
-getCommmentLength();
-
+validateStringLength();
